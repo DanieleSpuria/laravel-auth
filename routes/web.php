923809d1dfd271d67cbbf81c75f81176cf7783f0
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-06-15 16:21:02 +0200

    add DashboardController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Guest\PageController;
@@ -16,5 +17,12 @@ use App\Http\Controllers\Guest\PageController;
 */
 
 Route::get('/', [PageController::class, 'index'])->name('home');
+
+Route::middleware(['auth', 'verified'])
+                ->name('admin.')
+                ->prefix('admin')
+                ->group(function() {
+                  Route::get('/', [DashboardController::class, 'index'])->name('home');
+                });
 
 require __DIR__.'/auth.php';
